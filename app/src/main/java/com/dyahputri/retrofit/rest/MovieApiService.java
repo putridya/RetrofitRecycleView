0118f75ed133c61f4d6cf3ab91bc5efb9390d452
putridya <putridya99@users.noreply.github.com>
2019-04-29 10:56:24 +0700

    first commits

---
+++ app/src/main/java/com/dyahputri/retrofit/rest/MovieApiService.java
@@ -0,0 +1,12 @@
+package com.dyahputri.retrofit.rest;
+
+import com.dyahputri.retrofit.model.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieApiService {
+    @GET("movie/top_rated")
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+}
